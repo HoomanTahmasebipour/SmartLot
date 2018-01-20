4b2bb454a6225588f172dd52e9e3162a57f3c05a
Hooman <hooman_t_p@bitbucket.org>
2018-01-20 16:56:55 -0500

    updated documentation of project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SmartLot
 Used the DE1-Soc Board and the HC-SR04 sensor to replicate crash prevention technology in modern cars with a list of available spots inside a parking lot.
+
+The sensor's range is from 2 cm to 5 m, in order to make the range more useful, the user will be alreted of imminent crash when the distance between 
+the object and the sensor goes below 10 cm. When this happens a beeping sound is triggered from the speaker and the display shows that a car has filled
+up the remainng spot. As the object get closer to the sensor, the frequency of the beeps increase. If the object gets too close to the sensor (less than
+4 cm) the frequency of the beeps reaches its max and the screen begins to flash "Crash".
--- SmartLot.v
+++ SmartLot.v
@@ -1,5 +1,15 @@
 `timescale 1ns / 1ns // `timescale time_unit/time_precision
 
+
+// Top level module, all necessary I/O signals, wires, and modules are instantiated here.
+
+
+// The sensor's range is from 2 cm to 5 m, in order to make the range more useful, the user will be alreted of imminent crash when the distance between 
+// the object and the sensor goes below 10 cm. When this happens a beeping sound is triggered from the speaker and the display shows that a car has filled
+// up the remainng spot. As the object get closer to the sensor, the frequency of the beeps increase. If the object gets too close to the sensor (less than
+// 4 cm) the frequency of the beeps reaches its max and the screen begins to flash "Crash".
+
+
 module SmartLot (SW, CLOCK_50, GPIO_0, GPIO_1, HEX0, HEX1, HEX4, HEX5,  backgroundColour, AUD_ADCDAT, AUD_BCLK, AUD_ADCLRCK, AUD_DACLRCK, FPGA_I2C_SDAT, 
 					  AUD_XCK, AUD_DACDAT, FPGA_I2C_SCLK, VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_R, VGA_G, VGA_B);
 					  
